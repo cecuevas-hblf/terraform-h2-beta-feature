8071c7880882c39682896c0f677419aa05a8c051
cecuevas-hblf <carloscue-c@herbalife.com>
2025-01-22 16:18:52 -0600

    chore(main): release 0.3.1

--- versions.tf
+++ versions.tf
@@ -29,9 +29,9 @@ terraform {
   }
 
   provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-network:vpc/v0.3.0"
+    module_name = "blueprints/terraform/terraform-google-network:vpc/v0.3.1"
   }
   provider_meta "google-beta" {
-    module_name = "blueprints/terraform/terraform-google-network:vpc/v0.3.0"
+    module_name = "blueprints/terraform/terraform-google-network:vpc/v0.3.1"
   }
 }
